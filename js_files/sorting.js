bc42129ac5039777702f9bbbd94d3ec77bbf7073
Abhishek-Singh <abhishekbtp05@gmail.com>
2023-03-21 01:46:26 +0530

    first commit

---
+++ js_files/sorting.js
@@ -0,0 +1,26 @@
+
+// To create new array input size of array
+function createNewArray(noOfBars = 60) {
+
+
+
+    // creating an array of random numbers 
+    array = [];
+    for (let i = 0; i < noOfBars; i++) {
+        array.push(Math.floor(Math.random() * 250) + 1);
+    }
+    console.log(array);
+
+    // select the div #bars element
+    const bars = document.querySelector("#bars");
+
+
+    for (let i = 0; i < noOfBars; i++) {
+        const bar = document.createElement("div");
+        bar.style.height = `${array[i] * 2}px`;
+        bar.classList.add('bar');
+        bar.classList.add('flex-item');
+        bar.classList.add(`barNo${i}`);
+        bars.appendChild(bar);
+    }
+}
